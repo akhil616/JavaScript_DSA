fecef0d73dc649828b7712981f7048ad74f132e6
Akhil Chauhan <109506738+akhil616@users.noreply.github.com>
2025-12-14 14:50:30 +0530

    Create group anagram.js

---
+++ strings/group anagram.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let ans = {}
+    for (let s of strs) {
+        const count = new Array(26).fill(0);
+        for (let c of s) {
+            count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        }
+        let key = count.join('*');
+        if (!ans[key]) {
+            ans[key] = [];
+        }
+        ans[key].push(s);
+    }
+    return Object.values(ans);
+};
